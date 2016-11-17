e39664a97bed8cf4de9d4fff8fa08af30b61ab04
NguyenTTin <tinvt2.na@gmail.com>
2016-11-17 09:05:43 +0700

    Creating if_else.py module

---
+++ Basic_Syntax/If_else.py
@@ -0,0 +1,16 @@
+__auth__ = 'NguyenTTin'
+
+score = int(input("Enter the score:  "))
+if score >= 90:
+    print("The grade is A")
+elif score >=80:
+    print("The grade is B")
+elif score >=70:
+    print("The grade is C")
+elif score >=60:
+    print("The grade is D")
+elif score >=50:
+    print("The grade is F")
+else:
+    print("The grade is below F")
+
